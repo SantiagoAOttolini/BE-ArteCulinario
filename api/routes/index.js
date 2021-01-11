98f32a14bca965ed344a0a7bd950472ae88be128
santiago <santiago.ottolini98@gmail.com>
2021-01-11 18:24:24 -0300

    AC-BE: Refactorized security model

--- api/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const user = require('./user')
-
-const router = express.Router()
-
-router.use('/api/public/uploads', express.static('api/public/uploads'))
-
-router.use('/api/user', user)
-module.exports = router
