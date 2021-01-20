77463d2f31f2bd6cca8795bd620a554cd1f72b53
Facundo Loberse <facundosa123@gmail.com>
2021-01-20 02:14:57 -0300

    AC-BE: Added static route for food images

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,14 @@ const pay = require('./Pays/Pays')
 const router = express.Router()
 
 router.use('/public/uploads', express.static('server/public/uploads'))
+router.use(
+  '/public/images/beneficios',
+  express.static('server/public/images/beneficios')
+)
+router.use(
+  '/public/images/foods',
+  express.static('server/public/images/foods/')
+)
 
 router.use('/api/users', user)
 router.use('/api/food', food)
